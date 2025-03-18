2c5a0235088b522924536dfd58cc2cc91d70d1f2
adamkrystians <adamkrystian@yahoo.com>
2025-03-18 10:46:28 +0000

    Plus Composite added,

--- src/main/java/is/hi/hbv202g/ass9/composite/PlusComposite.java
+++ src/main/java/is/hi/hbv202g/ass9/composite/PlusComposite.java
@@ -1,6 +1,33 @@
 package is.hi.hbv202g.ass9.composite;
 
-public class PlusComposite {
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlusComposite implements MathExpression {
+
+    private List<MathExpression> children = new ArrayList<>();
+
+    public double getResult(){
+        double result = 0;
+
+        for (MathExpression expression : children) {
+            result += expression.getResult();
+        }
+        return result;
+    }
+
+    public void add(MathExpression expression){
+        children.add(expression);
+    }
+
+    public void remove(MathExpression expression){
+        children.remove(expression);
+    }
+
+    public List<MathExpression> getChildren(){
+        return children;
+    }
+
     public static void main(String[] args) {
 
     }
