a5497a523219f7fdd2dd7fc7b63c723b7e8e6418
Valurkristinn <109678364+Valurks@users.noreply.github.com>
2025-03-18 10:36:11 +0000

    classes created

---
+++ src/main/java/is/hi/hbv202g/ass9/composite/NumberLeaf.java
@@ -0,0 +1,14 @@
+package is.hi.hbv202g.ass9.composite;
+
+public class NumberLeaf {
+
+    private int value;
+
+    public NumberLeaf(int value) {
+        this.value = value;
+    }
+
+    public int getResult() {
+        return value;
+    }
+}
